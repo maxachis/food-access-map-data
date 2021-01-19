622cd780fea8bace264b2b4cff9c6d1d79112e35
Catalina Moreno <cmoreno@andrew.cmu.edu>
2021-01-19 18:49:13 -0500

    add last piece to create csv

--- data_prep_scripts/run.sh
+++ data_prep_scripts/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-Rscript auto_agg_clean_data.R ../food-data/Cleaned_data_files/ | Rscript auto_text_process_name.R | Rscript auto_geocode.R 'token' 1> ../merged_datasets.csv
+Rscript auto_agg_clean_data.R ../food-data/Cleaned_data_files/ | Rscript auto_text_process_name.R | Rscript auto_geocode.R 'token' | Rscript auto_write_to_csv.R
 
